b221a3ed41b6072350043f8c6db98ee37d05f85a
Nat-Nigro <natalenigro15@gmail.com>
2024-02-28 15:49:35 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using MVC_3.Models;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace MVC_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+            List<Prodotti> prodotto = new List<Prodotti>();
+
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Prodotti";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Prodotti prodotto2 = new Prodotti();
+                    prodotto2.NomeProdotto = reader.GetString(1);
+                    prodotto2.ImgCop = reader.GetString(4);
+                    prodotto.Add(prodotto2);
+                }
+            }
+            catch (SqlException e)
+            {
+                ViewBag.Message = e.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View(prodotto);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Prodotti.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC_3.Models
+{
+    public class Prodotti
+    {
+        public int IdProdotto { get; set; }
+        public string NomeProdotto { get; set; }
+        public decimal Prezzo { get; set; }
+        public string Descrizione { get; set; }
+        public string ImgCop { get; set; }
+        public string ImgAgg { get; set; }
+        public bool IsVisible { get; set; }
+
+    }
+}
